a88203db33bdef4d31b9831d9c48e3b16c5cdb97
Yaodong Zhao <git@yaodong.org>
2021-04-17 14:20:52 -0700

    clean up

--- lib/reorganize_string.rb
+++ lib/reorganize_string.rb
@@ -2,19 +2,20 @@ module ReorganizeString
 
   # @param [String] string
   def solve(string)
-    pq = heapify string
+    heap = heapify string
 
     # If the number of occurrences of some character is greater than (N + 1) / 2, the task is impossible.
-    return '' if pq.any? { |c| c.n > (string.size + 1) / 2 }
+    return '' if heap.any? { |c| c.freq > (string.size + 1) / 2 }
 
+    # pull out char with greatest freq
     ans = []
-    ans << next_char(pq) while pq.any?
+    ans << next_char(heap) while heap.any?
     ans.join
   end
 
-  Item = Struct.new(:char, :n) do
+  Item = Struct.new(:char, :freq) do
     def <=>(other)
-      n <=> other.n
+      freq <=> other.freq
     end
   end
 
@@ -23,15 +24,15 @@ module ReorganizeString
     counts.map { |k, v| Item.new(k, v) }.sort!
   end
 
-  def heap_push(arr, x)
-    idx = arr.bsearch_index { |y| (y <=> x) >= 0 } || arr.size
-    arr.insert(idx, x)
+  def heap_push(heap, item)
+    idx = heap.bsearch_index { |y| (y <=> item) >= 0 } || heap.size
+    heap.insert(idx, item)
   end
 
-  def next_char(pq)
-    item = pq.pop
-    item.n -= 1
-    heap_push(pq, item) if item.n.positive?
+  def next_char(heap)
+    item = heap.pop
+    item.freq -= 1
+    heap_push(heap, item) if item.freq.positive?
     item.char
   end
 end
